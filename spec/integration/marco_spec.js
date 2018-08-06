59941b75e54e266cc988bb5f3c856697d9fe513d
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-08-06 17:52:14 -0500

    troubleshooting

---
+++ spec/integration/marco_spec.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : marco", () => {
+  describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("should have 'polo' as the body", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(body).toContain("polo");
+        done();
+      });
+    });
+
+  });
+});
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,5 +2,7 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     app.use(staticRoutes);
+    const marcoRoute = require("../routes/marco");
+    app.use(marcoRoute);
   }
 }
