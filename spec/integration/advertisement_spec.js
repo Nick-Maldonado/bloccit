99a3ef9b317b55cf886c80640b762b9f17903a58
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-09-20 19:03:42 -0500

    fixed specs

--- spec/integration/advertisement_spec.js
+++ spec/integration/advertisement_spec.js
@@ -1,8 +1,27 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/advertisement/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Advertisement = require("../../src/db/models").Advertisement;
 
 describe("routes : advertisement", () => {
+  beforeEach((done) => {
+    this.advertisement;
+    sequelize.sync({force: true}).then((res) => {
+     Advertisement.create({
+       title: "JS Frameworks",
+       description: "There is a lot of them"
+     })
+      .then((advertisement) => {
+        this.advertisement = advertisement;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+  });
 
   describe("GET /advertisement", () => {
 
@@ -11,6 +30,122 @@ describe("routes : advertisement", () => {
         expect(res.statusCode).toBe(200);
         done();
       });
+    });
+  });
+
+  describe("GET /advertisement/new", () => {
+
+    it("should render a new advertisement form", (done) => {
+      request.get(`${base}new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Advertisement");
+        done();
+      });
+    });
+
+  });
+
+  describe("POST /advertisement/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "blink-182 songs",
+        description: "What's your favorite blink-182 song?"
+      }
+    };
+
+    it("should create a new advertisement and redirect", (done) => {
+      request.post(options,
+        (err, res, body) => {
+          Advertisement.findOne({where: {title: "blink-182 songs"}})
+          .then((advertisement) => {
+            expect(res.statusCode).toBe(303);
+            expect(advertisement.title).toBe("blink-182 songs");
+            expect(advertisement.description).toBe("What's your favorite blink-182 song?");
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+  });
+
+  describe("GET /advertisement/:id", () => {
+
+    it("should render a view with the selected advertisement", (done) => {
+      request.get(`${base}${this.advertisement.id}`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("JS Frameworks");
+        done();
+      });
+    });
+
+  });
+
+  describe("POST /advertisement/:id/destroy", () => {
+
+    it("should delete the advertisement with the associated ID", (done) => {
+      Advertisement.all()
+      .then((advertisement) => {
+        const advertisementCountBeforeDelete = advertisement.length;
+
+         expect(advertisementCountBeforeDelete).toBe(1);
+
+         request.post(`${base}${this.advertisement.id}/destroy`, (err, res, body) => {
+          Advertisement.all()
+          .then((advertisement) => {
+            expect(err).toBeNull();
+            expect(advertisement.length).toBe(advertisementCountBeforeDelete - 1);
+            done();
+          })
+
+        });
+      });
+
+    });
+
+  });
+
+  describe("GET /advertisement/:id/edit", () => {
+
+    it("should render a view with an edit advertisement form", (done) => {
+      request.get(`${base}${this.advertisement.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Advertisement");
+        expect(body).toContain("JS Frameworks");
+        done();
+      });
+    });
+
+  });
+
+  describe("POST /advertisement/:id/update", () => {
+
+    it("should update the advertisement with the given values", (done) => {
+       const options = {
+          url: `${base}${this.advertisement.id}/update`,
+          form: {
+            title: "JavaScript Frameworks",
+            description: "There are a lot of them"
+          }
+        };
+
+        request.post(options,
+          (err, res, body) => {
+
+          expect(err).toBeNull();
+          
+          Advertisement.findOne({
+            where: { id: this.advertisement.id }
+          })
+          .then((advertisement) => {
+            expect(advertisement.title).toBe("JavaScript Frameworks");
+            done();
+          });
+        });
     });
 
   });
