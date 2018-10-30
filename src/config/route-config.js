fcbbded9b985197a157de5f42d4cf9c652969721
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-10-30 17:21:55 -0500

    authentication checkpoint complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,11 +5,13 @@ module.exports = {
     const aboutRoute = require("../routes/about");
     const topicRoutes = require("../routes/topics");
     const postRoutes = require("../routes/posts");
+    const userRoutes = require("../routes/users");
 
     app.use(marcoRoute);
     app.use(staticRoutes);
     app.use(aboutRoute);
     app.use(topicRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
   }
 }
